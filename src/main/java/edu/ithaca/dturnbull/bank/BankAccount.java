2a4198f32d36b67450fb389ee49aae597de2f840
emmettbarry <emmettbarry@users.noreply.github.com>
2023-01-30 10:05:13 -0500

    finished implementing BankAccount.java

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -1,5 +1,6 @@
 package edu.ithaca.dturnbull.bank;
 
+
 public class BankAccount {
 
     private String email;
@@ -26,25 +27,71 @@ public class BankAccount {
         return email;
     }
 
+    public static boolean isAmountValid(double amount) {
+        // method takes in a double and checks amount of decimal points
+        if (amount < 0) {
+            return false;
+        }
+        if ((amount * 100) % 1 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance){
-            balance -= amount;
-        }
-        else {
-            throw new InsufficientFundsException("Not enough money");
+        if (isAmountValid(amount)) {
+            if (amount <= balance) {
+                balance -= amount;
+            } else {
+                throw new InsufficientFundsException("Not enough money");
+            }
+        } else {
+            throw new InsufficientFundsException("Amount Not Valid");
         }
     }
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+        int atsign = email.indexOf('@');
+        if (atsign == -1){
             return false;
         }
-        else {
-            return true;
+     
+        for(int i=0; i<atsign; i++ ){
+            if(email.charAt(i) == '#'){
+                return false;
+            }
+            if (email.charAt(i) == '-' || email.charAt(i) == '_' || email.charAt(i) == '.'){
+                
+                if (i == atsign-1){
+                    return false;
+                }
+                if (i == 0){
+                    return false;
+                }
+                if (email.charAt(i+1) == '-' || email.charAt(i+1) == '_' || email.charAt(i+1) == '.'){
+                    return false;
+                }
+                if (email.charAt(i-1) == '-' || email.charAt(i-1) == '_' || email.charAt(i-1) == '.'){
+                    return false;
+                }
+                
+            }
         }
+        for (int i=atsign; i<email.length(); i++){
+            if(email.charAt(i) == '#'){
+                return false;
+            }
+            if (email.charAt(i) == '.'){
+                
+            }
+        }
+    
+        return true;
+       
     }
 }
